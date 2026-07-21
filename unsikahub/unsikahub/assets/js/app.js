ff5eb337fc05c901673773da437fb23ac0d97533
Dyka0 <andhika.muldani07@gmail.com>
2026-07-21 23:11:57 +0700

    Delete unsikahub/unsikahub directory

--- unsikahub/unsikahub/assets/js/app.js
+++
@@ -1,7 +0,0 @@
-document.querySelectorAll('[data-confirm]').forEach((button) => {
-  button.addEventListener('click', (event) => {
-    if (!confirm(button.dataset.confirm || 'Lanjutkan tindakan ini?')) {
-      event.preventDefault();
-    }
-  });
-});
